4772c8b2759c8873729c3006cca6804995bf7fed
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-07 12:47:10 +0600

    Created 2types of Array Declare system

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Indexed Arrays
+You can create an indexed array using the array() function or the short array syntax[].
+
+*/
+
+$array1=["Dhaka", 'Noakhali', 'Khulna'];
+
+$array2=array('Dhaka','Noakhali','khulna');
+
