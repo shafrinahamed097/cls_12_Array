9e2aa541e301baad6da1a13023e43a7c1fbe6a47
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-08 17:35:20 +0600

    1.1 Declare Array Push

--- array_combine.php
+++ array_combine.php
@@ -5,4 +5,3 @@ $key=["a", "b", "c"];
 $value=["apple", "ball", "cat"];
 
 $array_combine=array_combine($value, $key);
-print_r($array_combine);
---
+++ array_push.php
@@ -0,0 +1,7 @@
+<?php 
+
+$countries = ['Bangladesh', 'India'];
+array_push($countries, 'Pakistan');
+
+print_r($countries);
+
