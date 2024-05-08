3eb37f3a881289fac11d9a64b662516c3f28e486
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-08 16:26:56 +0600

    Declare ForEach loop through an multidimensional array

--- multidimensional_array.php
+++ multidimensional_array.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // A multidimensional array is an array that contains one or more arrays.
 
 
 
-$arr=[
-   ['a','b','c','d'],
-   ['e','f','g'],
-   ['h','i','j']
+$arr = [
+    ['a', 'b', 'c', 'd'],
+    ['e', 'f', 'g'],
+    ['h', 'i', 'j']
 
 ];
 
@@ -14,12 +14,20 @@ $arr=[
 
 // echo $arr[1][1];
 
-// Multi Dimensional Asscoiative Array  
+// Multi Dimensional Associative Array  
 
-$arr1=[
-    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
-    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
-    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
+$arr1 = [
+    ['firstName' => 'shafrin', 'lastName' => 'Ahamed', 'age' => 27],
+    ['firstName' => 'shafrin', 'lastName' => 'Ahamed', 'age' => 27],
+    ['firstName' => 'shafrin', 'lastName' => 'Ahamed', 'age' => 27],
 ];
 
-echo $arr1[0]['firstName'], $arr1[0]['lastName'];
+// echo $arr1[0]['firstName'], $arr1[0]['lastName'];
+
+// ForEach Loop Through an Multidimensional Array
+
+foreach($arr1 as $child){
+    foreach($child as $key=>$value){
+        echo "$value" ."\n";
+    }
+}
