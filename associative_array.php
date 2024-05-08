9299ee6779f9f6c23955f7f520a0dc966f63c3d1
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-08 12:24:04 +0600

    38 create Multidimentional array

--- associative_array.php
+++ associative_array.php
@@ -2,12 +2,8 @@
 
 // Associative Array is Like JS Object key:Value Pair
 
-$person=["name" =>"Shafrin Ahamed", "age" =>27, "city"=>"Noakhali"];
+$person=["name" =>"Shafrin ahamed", "age" => 27, "city"=>"Noakhali"];
 
-
-
-// apply foreach this array
-
-foreach($person as $key => $value){
+foreach($person as $key =>$value){
     echo $key . " " . $value ."\n";
 }
---
+++ multidimensional_array.php
@@ -0,0 +1,8 @@
+<?php 
+// A multidimensional array is an array that contains one or more arrays.
+
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+$array3 = [7, 8, 9];
+
+$arr=[$array1, $array2, $array3];
