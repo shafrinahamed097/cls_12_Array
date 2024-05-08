54444d232784c1d32887ba6ab1a50b3cd2c4d823
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-08 14:00:19 +0600

    Declare Multi Dimensional Associative array

--- multidimensional_array.php
+++ multidimensional_array.php
@@ -1,8 +1,25 @@
 <?php 
 // A multidimensional array is an array that contains one or more arrays.
 
-$array1 = [1, 2, 3];
-$array2 = [4, 5, 6];
-$array3 = [7, 8, 9];
 
-$arr=[$array1, $array2, $array3];
+
+$arr=[
+   ['a','b','c','d'],
+   ['e','f','g'],
+   ['h','i','j']
+
+];
+
+// Accessing values
+
+// echo $arr[1][1];
+
+// Multi Dimensional Asscoiative Array  
+
+$arr1=[
+    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
+    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
+    ['firstName'=>'shafrin' ,'lastName'=>'Ahamed', 'age' =>27],
+];
+
+echo $arr1[0]['firstName'], $arr1[0]['lastName'];
