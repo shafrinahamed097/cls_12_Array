3bfcf866f07a6c8c4176d3afd00496086f2a71e0
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-09 12:32:45 +0600

    Declare in_array & is_array method

---
+++ in_array.php
@@ -0,0 +1,6 @@
+<?php 
+
+// in_array() - Check if a value exists in an array
+$arr=[1,2,3,4,5,6];
+$check=in_array(5, $arr);
+echo $check;
---
+++ is_array.php
@@ -0,0 +1,7 @@
+<?php
+
+// is_array() - check if a variable is an array
+
+$arr=[1,2,2,4];
+$check=is_array($arr);
+echo $check;
